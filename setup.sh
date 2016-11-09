da725042aa35ceceb7911fe76bb11ca1cb8cb66c
Craig Blaszczyk <masterjakul@gmail.com>
2016-11-09 10:25:56 +0000

    copy gitconfig to prevent pycharm from being upset

--- setup.sh
+++ setup.sh
@@ -1,20 +1,32 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-FILES=(
+SYMLINKS=(
     ".bash_profile"
     ".bash_prompt"
     ".django_bash_completion"
     ".fabric_completion.bash"
-    ".gitconfig"
     ".gitignore"
     ".ngrok2"
 )
-for i in "${FILES[@]}"
+COPY=(
+    ".gitconfig"
+)
+for i in "${SYMLINKS[@]}"
 do
     if [ -e ~/$i ]; then
       echo "=> ~/$i already exists"
     else
       ln -s $DIR/$i ~/$i
       echo "=> symlinked ~/$i"
+    fi
+done
+
+for i in "${COPY[@]}"
+do
+    if [ -e ~/$i ]; then
+      echo "=> ~/$i already exists"
+    else
+      cp $DIR/$i ~/$i
+      echo "=> copy ~/$i"
     fi
 done
